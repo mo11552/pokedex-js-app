deb39dc860d34bfdd797096f7ff56a591e4af515
Moyo Olojede <mo11552@icloud.com>
2023-03-14 09:48:45 -0400

    closing square bracket added in 2nd object

--- js/scripts.js
+++ js/scripts.js
@@ -1,7 +1,7 @@
 //array of pokemon data
 let pokemonList = [
 { name: 'bulbasaur', height: 0.7, types:['grass','poison']},
-{ name: 'squirtle', height: 0.5, types:['fire'},
-{ name: 'charmander', height: 0.6 , types:'water'}
+{ name: 'squirtle', height: 0.5, types:['fire']},
+{ name: 'charmander', height: 0.6, types:'water'}
 ];
 
