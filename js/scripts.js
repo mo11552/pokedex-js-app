7cde6ac5ac826ed7913b314def6a0566c8a38b28
Moyo Olojede <mo11552@icloud.com>
2023-03-21 16:13:59 -0400

    pokemon names capitalized,  if statement changed and line break added

--- js/scripts.js
+++ js/scripts.js
@@ -1,14 +1,15 @@
 //array of pokemon data
 let pokemonList = [
-{ name: 'bulbasaur', height: 0.7, types:['grass','poison']},
-{ name: 'squirtle', height: 0.5, types:['fire']},
-{ name: 'charmander', height: 0.6, types:'water'}
+{ name: 'Bulbasaur', height: 0.7, types:['grass','poison']},
+{ name: 'Squirtle', height: 0.5, types:['fire']},
+{ name: 'Charmander', height: 0.6, types:'water'}
 ];
 
 
 for (let i = 0; i < pokemonList.length; i++) {
-  if(pokemonList[i].height > 0.5) {
-  	document.write("Wow thats big");
+	document.write(pokemonList[i].name + ' ' + 'height' + ' ' + pokemonList[i].height)
+  if(pokemonList[i].height > 0.6) {
+  	document.write(" - Wow that's big!");
   }
-  document.write(pokemonList[i].name + ' ' + 'height' + ' ' + pokemonList[i].height)
+  document.write("<br>");
 }
