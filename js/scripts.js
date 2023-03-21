dd1948b3a1e89021539bcfd6725b6d11e06caca7
Moyo Olojede <mo11552@icloud.com>
2023-03-21 15:53:44 -0400

    code changed from <= to <

--- js/scripts.js
+++ js/scripts.js
@@ -6,6 +6,6 @@ let pokemonList = [
 ];
 
 
-for (let i = 0; i <= pokemonList.length; i++) {
+for (let i = 0; i < pokemonList.length; i++) {
   document.write(pokemonList[i].name + ' ' + 'height' + ' ' + pokemonList[i].height)
 }
